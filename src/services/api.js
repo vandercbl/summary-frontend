842f558f3c0216a8a42de9400755b2008f177b0d
Vander Lima <vandercbl@gmail.com>
2020-04-13 21:51:19 -0300

    Adicionando variável de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: 'http://localhost:3333', //caminho da URL da nossa api, como estou no localhost será esse
+	baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api
